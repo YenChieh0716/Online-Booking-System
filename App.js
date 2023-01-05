5774afc78190033ddbe3bee1732f020589ebe6e7
phoebbbbe <leelemons1999@gmail.com>
2023-01-06 00:42:02 +0800

    1. 調整命名 2.移除用不到檔案

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signin from './pages/Signin';
-import Navbar from './navbar';
+import "./App.css";
+import Header from "./header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Navbar />
       <Routes>
-        <Route path='/' exact element={'首頁'}></Route>
-        <Route path='/Signin' exact element={<Signin />}></Route>
+        <Route path="/" exact element={"首頁"}></Route>
+        <Route path="/Signin" exact element={<Signin />}></Route>
       </Routes>
     </BrowserRouter>
   );
