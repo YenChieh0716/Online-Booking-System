ddf497580d13a879b01027ffdff697fd0bdc5a07
YenChieh0716 <t108590037@ntut.org.tw>
2023-01-06 02:03:07 +0800

    Revert "1. 調整命名 2.移除用不到檔案"
    This reverts commit 5774afc78190033ddbe3bee1732f020589ebe6e7.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import "./App.css";
-import Header from "./header";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./pages/Signin";
-import Navbar from "./components/Navbar";
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Navbar from './navbar';
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Navbar />
       <Routes>
-        <Route path="/" exact element={"首頁"}></Route>
-        <Route path="/Signin" exact element={<Signin />}></Route>
+        <Route path='/' exact element={'首頁'}></Route>
+        <Route path='/Signin' exact element={<Signin />}></Route>
       </Routes>
     </BrowserRouter>
   );
