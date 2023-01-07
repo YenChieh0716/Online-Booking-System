c760eb133a03b19c4e10d81aee58ddfc952b0255
YenChieh0716 <79859588+YenChieh0716@users.noreply.github.com>
2023-01-07 14:38:31 +0800

    Update App.js

--- full-react-app/src/App.js
+++ full-react-app/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import Header from "./header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -14,29 +13,6 @@ function App() {
         <Route path='/Signin' exact element={<Signin />}></Route>
       </Routes>
     </BrowserRouter>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
->>>>>>> ec7df7f (Initialize project using Create React App)
   );
 }
 
