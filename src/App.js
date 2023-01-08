9639261523045b3bd09e7d9bf89bfd6653753240
YenChieh0716 <t108590037@ntut.org.tw>
2023-01-08 21:15:51 +0800

    Revert "delete unused files"
    This reverts commit d7d175a034827aa4a39f0a5fe12b6879c1995400.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Navbar from "./navbar";
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={'首頁'}></Route>
+        <Route path='/Signin' exact element={<Signin />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
